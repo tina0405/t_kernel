8332395cee47176d592000b3b41baacb267d7985
tina0405 <tina423424@gmail.com>
2018-11-07 13:13:18 +0800

    Use COM3 print imformation with UART

--- drivers/serial_port.c
+++ drivers/serial_port.c
@@ -1,5 +1,5 @@
 #include <serial_port.h>
-int calc_divisor (NS16550_t port)
+int calc_divisor (void)
 {
 	return (CFG_NS16550_CLK / 16 /115200);
 }
@@ -7,38 +7,38 @@ int calc_divisor (NS16550_t port)
 void t_uart_puts (const char *s)
 {
 	while(*s){
-	     NS16550_putc(serial_ports[2],*s++);
+	     NS16550_putc(*s++);
 	}
 }
 
-void NS16550_putc (NS16550_t com_port, char c)
+void NS16550_putc (char c)
 {
 	if (c == '\n')
-		NS16550_putc (serial_ports[2], '\r');
-	while (( com_port->lsr & LSR_THRE) == 0);
-	com_port->thr = c;
+		NS16550_putc ('\r');
+	while ((serial_ports[2]->lsr & LSR_THRE) == 0);
+	serial_ports[2]->thr = c;
 }
 
-char NS16550_getc (NS16550_t com_port)
+char NS16550_getc (void)
 {
-	while ((com_port->lsr & LSR_DR) == 0);
-	return (com_port->rbr);
+	while ((serial_ports[2]->lsr & LSR_DR) == 0);
+	return (serial_ports[2]->rbr);
 }
 
-int NS16550_tstc (NS16550_t com_port)
+int NS16550_tstc (void)
 {
-	return ((com_port->lsr & LSR_DR) != 0);
+	return ((serial_ports[2]->lsr & LSR_DR) != 0);
 }
 
-void NS16550_reinit (NS16550_t com_port, int baud_divisor)
+void NS16550_reinit (int baud_divisor)
 {
-	com_port->ier = 0x00;
-	com_port->lcr = LCR_BKSE;
-	com_port->dll = baud_divisor & 0xff;
-	com_port->dlm = (baud_divisor >> 8) & 0xff;
-	com_port->lcr = LCRVAL;
-	com_port->mcr = MCRVAL;
-	com_port->fcr = FCRVAL;
+	serial_ports[2]->ier = 0x00;
+	serial_ports[2]->lcr = LCR_BKSE;
+	serial_ports[2]->dll = baud_divisor & 0xff;
+	serial_ports[2]->dlm = (baud_divisor >> 8) & 0xff;
+	serial_ports[2]->lcr = LCRVAL;
+	serial_ports[2]->mcr = MCRVAL;
+	serial_ports[2]->fcr = FCRVAL;
 }
 
 
